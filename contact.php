8801fadf92f761ab77d4100212228cd72fd22c8f
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2022-08-21 09:27:04 +0700

    make a main, contact, and news page

---
+++ contact.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact - AniSuki</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> -->
+
+    <link rel="stylesheet" href="style.css">
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header>
+        <div id="nav-scroll">
+            <nav class="navbar navbar-expand-lg navbar-light py-4 bgcolor" data-aos="fade-down">
+                <div class="container">
+                    <a class="navbar-brand" href="#"><span>Ani</span>Suki</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <div class="navbar-nav ms-auto">
+                            <a class="nav-link" aria-current="page" href="nime.html">ANIME</a>
+                            <a class="nav-link" href="#">SCHEDULE</a>
+                            <a class="nav-link" href="news.php">NEWS</a>
+                            <a class="nav-link active" href="contact.php">CONTACT US</a>
+                            <a class="nav-link btn btn-info px-4 text-white" href="list.php">My List</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <section class="contact mt-4">
+        <div class="container">
+            <div class="row pt-5" data-aos="fade-up" data-aos-once="true">
+                <div class="col-12">
+                    <h2 class="section-heading">CONTACT US</h2>
+                </div>
+                <div class="form mt-5 mb-5">
+                    <form action="">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="row">
+                                    <div class="col-12 mb-3" data-aos="fade-up" data-aos-once="true" data-aos-delay="100">
+                                        <label for="" class="form-label">Name</label>
+                                        <input type="text" class="form-control" required>
+                                    </div>
+                                    <div class="col-12 mb-3" data-aos="fade-up" data-aos-once="true" data-aos-delay="200">
+                                        <label for="" class="form-label">Email</label>
+                                        <input type="email" class="form-control" id="inputEmail4" required>
+                                    </div>
+                                    <div class="col-12 mb-3" data-aos="fade-up" data-aos-once="true" data-aos-delay="300">
+                                        <div class="col-12"><label for="" class="form-label">Topik</label></div>
+                                        <div class="col-12">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                Bantuan
+                                            </label>
+                                        </div>
+                                        <div class="col-12">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                Informasi
+                                            </label>
+                                        </div>
+                                        <div class="col-12">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                Laporan Kesalahan Berita
+                                            </label>
+                                        </div>
+                                        <div class="col-12">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                Lainnya
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6" data-aos="fade-up" data-aos-once="true" data-aos-delay="400">
+                                <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="9"></textarea>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-12">
+                                    <button type="submit" name="submit" class="button">
+                                        <p>Submit</p>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="footer mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-5">
+                    <div class="logo-footer">
+                        <a class="navbar-brand" href="index.html"><span>Portal</span>nime</a>
+                    </div>
+                    <div class="info mt-4">
+                        <div class="footer-text mt-3">
+                            <p class="text-white">Portal Japan adalah sebuah media yang membahas berita-berita
+                                yang berkaitan dengan Jepang, baik itu culture maupun pop-culture, mulai dari yang unik,
+                                aneh hingga yang luar biasa penting.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4"></div>
+                <div class="col-2">
+                    <div class="info mt-4">
+                        <h5>Hubungi Kami</h5>
+                        <div class="sosmed">
+                            <div class="row mt-3">
+                                <div class="col-12">
+                                    <a href="mailto: portaljapan@gmail.com" target="_blank" class=" infotxt">portalnime@gmail.com</a>
+                                </div>
+                                <div class="col-12">
+                                    <a href="#" class="infotxt">0896-3091-5828</a>
+                                </div>
+                                <div class="col-12 mt-2">
+                                    <a href="#"><i class="bx bxl-whatsapp pe-2"></i></a>
+                                    <a href="#"><i class='bx bxl-instagram px-2'></i></a>
+                                    <a href="#"><i class='bx bxl-facebook-circle ps-2'></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1"></div>
+            </div>
+            <div class="row">
+                <div class="col-12 mt-4">
+                    <p class="copyright">Copyright © 2022 Razin Syakib | All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Footer -->
+        <section class="footer mt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-5">
+                        <div class="logo-footer">
+                            <a class="navbar-brand" href="index.html"><span>Ani</span>Suki</a>
+                        </div>
+                        <div class="info mt-4">
+                            <div class="footer-text mt-3">
+                                <p class="text-white">Portal Japan adalah sebuah media yang membahas berita-berita
+                                    yang berkaitan dengan Jepang, baik itu culture maupun pop-culture, mulai dari yang unik,
+                                    aneh hingga yang luar biasa penting.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4"></div>
+                    <div class="col-2">
+                        <div class="info">
+                            <h5 style="color: #FBFBFB;">Hubungi Kami</h5>
+                            <div class="sosmed">
+                                <div class="row mt-3">
+                                    <div class="col-12">
+                                        <a href="mailto: anisuki@gmail.com" target="_blank" class=" infotxt">anisuki@gmail.com</a>
+                                    </div>
+                                    <div class="col-12">
+                                        <a href="#" class="infotxt">0896-3091-5828</a>
+                                    </div>
+                                    <div class="col-12 mt-2">
+                                        <a href="#"><i class="bx bxl-whatsapp pe-2"></i></a>
+                                        <a href="#"><i class='bx bxl-instagram px-2'></i></a>
+                                        <a href="#"><i class='bx bxl-facebook-circle ps-2'></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-1"></div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mt-4">
+                        <p class="copyright">Copyright © 2022 Razin Syakib | All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer -->
+
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
+        <script>
+            AOS.init();
+        </script>
+</body>
+
+</html>
---
+++ news.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Pertemuan ke 3</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> -->
+
+    <link rel="stylesheet" href="style.css">
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header>
+        <div id="nav-scroll">
+            <nav class="navbar navbar-expand-lg navbar-light py-4 bgcolor" data-aos="fade-down">
+                <div class="container">
+                    <a class="navbar-brand" href="#"><span>Ani</span>Suki</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <div class="navbar-nav ms-auto">
+                            <a class="nav-link" aria-current="page" href="nime.html">ANIME</a>
+                            <a class="nav-link" href="#">SCHEDULE</a>
+                            <a class="nav-link active" href="news.php">NEWS</a>
+                            <a class="nav-link" href="contact.php">CONTACT US</a>
+                            <a class="nav-link btn btn-info px-4 text-white" href="list.php">My List</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <section class="article mt-4">
+        <div class="container">
+            <div class="row">
+                <div class="sidebar col-4 mt-5">
+                    <div class="row">
+                        <h5 class="section-sidebar" data-aos="fade-up" data-aos-once="true" data-aos-delay="100">POPULER
+                        </h5>
+                    </div>
+                    <div class="row" data-aos="fade-up" data-aos-once="true" data-aos-delay="200">
+                        <div class="col-6">
+                            <a href="#"><img src="img/sidebarnews.png" alt=""></a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#">
+                                <p class="sidebar-heading">Klinik Di Tokyo Dipenuhi Pasien Di Tengah Lonjakan Kasus
+                                    COVID
+                                </p>
+                            </a>
+                            <a href="#">
+                                <p class="sidebar-tanggal">23 July 2022</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row mt-3" data-aos="fade-up" data-aos-once="true" data-aos-delay="300">
+                        <div class="col-6">
+                            <a href="#"><img src="img/sidebarnews2.jpg" alt=""></a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#">
+                                <p class="sidebar-heading">NASA Keluarkan Foto Pertama Dari Teleskop Antariksa Baru
+                                </p>
+                            </a>
+                            <a href="#">
+                                <p class="sidebar-tanggal">23 July 2022</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row mt-3" data-aos="fade-up" data-aos-once="true" data-aos-delay="400">
+                        <div class="col-6">
+                            <a href="#"><img src="img/sidebarnews3.png" alt=""></a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#">
+                                <p class="sidebar-heading">Survei Jepang: 40% Responden Beri Bantuan Kepada Ukraina
+                                </p>
+                            </a>
+                            <a href="#">
+                                <p class="sidebar-tanggal">23 July 2022</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row mt-3" data-aos="fade-up" data-aos-once="true" data-aos-delay="500">
+                        <div class="col-6">
+                            <a href="#"><img src="img/sidebarnews4.jpg" alt=""></a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#">
+                                <p class="sidebar-heading">Pemerintah Mungkin Kurangi Isolasi Kontak Dekat Jadi 5 Hari
+                                </p>
+                            </a>
+                            <a href="#">
+                                <p class="sidebar-tanggal">23 July 2022</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="content col-8 mt-5">
+                    <div class="row">
+                        <div class="col-12" data-aos="fade-up" data-aos-once="true" data-aos-delay="100">
+                            <h2 class="section-heading">Jepang Akan Gelar Pemakaman Kenegaraan Shinzo Abe 27 September
+                            </h2>
+                            <p class="subheading">Published by Razin Syakib on 23 July 2022 - 13:24 WIB</p>
+                        </div>
+                    </div>
+                    <div class="row" data-aos="fade-up" data-aos-once="true" data-aos-delay="200">
+                        <div class="col"></div>
+                        <div class="col">
+                            <img src="img/shinzonews.jpeg" alt="">
+                        </div>
+                        <div class="col"></div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col"></div>
+                        <div class="col-10" data-aos="fade-up" data-aos-once="true" data-aos-delay="300">
+                            <div class="paragraph mt-2">
+                                <p class="subheading-article">Jepang akan menggelar pemakaman kenegaraan untuk mendiang
+                                    mantan perdana menteri Shinzo Abe pada 27 September mendatang. Pemerintah Jepang
+                                    mengumumkan bahwa upacara pemakaman tersebut bakal digelar di Nippon Budokan,
+                                    kawasan
+                                    yang kerap dijadikan tempat untuk acara konser dan olahraga bergengsi.
+                                </p>
+                                <p class="subheading-article">Nippon Budokan juga merupakan tempat pemakaman kenegaraan
+                                    terakhir di Jepang digelar pada 1967. Saat itu, Jepang berduka ditinggal mantan PM
+                                    Shigaru Yoshida. Sepanjang sejarah, Jepang baru satu kali menggelar pemakaman
+                                    kenegaraan
+                                    bagi mantan PM
+                                    yang sudah tidak menjabat, yaitu Yoshida. Kala itu, Jepang ingin melepas Yoshida
+                                    dengan
+                                    hormat karena politikus itulah yang memimpin Jepang keluar dari kesulitan setelah
+                                    Perang
+                                    Dunia II. Kini, Jepang juga ingin memberikan penghormatan tertinggi bagi Abe,
+                                    politikus
+                                    ulung yang
+                                    menjadi PM terlama dalam sejarah Jepang.
+                                </p>
+                                <p class="subheading-article">Sebagaimana dilansir AFP, juru bicara pemerintah Jepang,
+                                    Hirokazu Matsuno, mengatakan bahwa selama berkiprah di kancah politik, Abe meraih
+                                    sejumlah pencapaian "yang sangat patut dipuji." Jepang pun bakal mengundang
+                                    perwakilan
+                                    luar negeri untuk memberikan penghormatan terakhir kepada Abe. Rencana upacara
+                                    kenegaraan ini menuai kritik sejumlah pihak yang mempertanyakan penggunaan uang
+                                    negara
+                                    untuk pemakaman seorang mantan pemimpin.
+                                </p>
+                                <p class="subheading-article">Selain itu, Abe juga sudah dimakamkan pada 12 Juli lalu
+                                    dengan
+                                    upacara yang digelar keluarga. Namun, Matsuno memastikan bahwa pemakaman kenegaraan
+                                    ini
+                                    digelar dengan sederhana di tengah suasana berduka.
+                                </p>
+                                <p class="subheading-article">Abe meninggal dunia setelah ditembak oleh Tetsuya Yamagami
+                                    ketika sedang berpidato di Kota Nara pada 8 Juli lalu. Ia sempat dilarikan ke rumah
+                                    sakit, tapi pendarahan yang begitu parah membuat nyawanya tak terselamatkan. Abe
+                                    akhirnya mengembuskan napas terakhirnya pada 17.03 waktu setempat, meninggalkan
+                                    jejak
+                                    kegemilangan masa kepemimpinannya di Jepang.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer -->
+    <section class="footer mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-5">
+                    <div class="logo-footer">
+                        <a class="navbar-brand" href="index.html"><span>Ani</span>Suki</a>
+                    </div>
+                    <div class="info mt-4">
+                        <div class="footer-text mt-3">
+                            <p class="text-white">Portal Japan adalah sebuah media yang membahas berita-berita
+                                yang berkaitan dengan Jepang, baik itu culture maupun pop-culture, mulai dari yang unik,
+                                aneh hingga yang luar biasa penting.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4"></div>
+                <div class="col-2">
+                    <div class="info">
+                        <h5 style="color: #FBFBFB;">Hubungi Kami</h5>
+                        <div class="sosmed">
+                            <div class="row mt-3">
+                                <div class="col-12">
+                                    <a href="mailto: anisuki@gmail.com" target="_blank" class=" infotxt">anisuki@gmail.com</a>
+                                </div>
+                                <div class="col-12">
+                                    <a href="#" class="infotxt">0896-3091-5828</a>
+                                </div>
+                                <div class="col-12 mt-2">
+                                    <a href="#"><i class="bx bxl-whatsapp pe-2"></i></a>
+                                    <a href="#"><i class='bx bxl-instagram px-2'></i></a>
+                                    <a href="#"><i class='bx bxl-facebook-circle ps-2'></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1"></div>
+            </div>
+            <div class="row">
+                <div class="col-12 mt-4">
+                    <p class="copyright">Copyright © 2022 Razin Syakib | All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script>
+        AOS.init();
+    </script>
+</body>
+
+</html>
